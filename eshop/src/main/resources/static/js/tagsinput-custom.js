ba6a28e1c4d78ef71393e2938d06e695c31fdfb4
Bui Huy Hoang <hoangbh2709@gmail.com>
2023-11-25 17:01:38 +0700

    Sửa một số giao diện dư thừa

--- eshop/src/main/resources/static/js/tagsinput-custom.js
+++ eshop/src/main/resources/static/js/tagsinput-custom.js
@@ -1,20 +1,18 @@
 /**
- * 
+ *
  */
 
-$(document).ready(function(){	
-		
-	$(".btn-size").click(function() {	
-		var val = $(this).text();		
-		$("#sizes").tagsinput('add', val);
-	});
-	$(".btn-type").click(function() {
-		var val = $(this).text();
-		$("#types").tagsinput('add', val);
-	});
-	$(".btn-category").click(function() {
-		var val = $(this).text();
-		$("#categories").tagsinput('add', val);
-	});
-	
+$(document).ready(function () {
+  $(".btn-color").click(function () {
+    var val = $(this).text();
+    $("#sizes").tagsinput("add", val);
+  });
+  $(".btn-type").click(function () {
+    var val = $(this).text();
+    $("#types").tagsinput("add", val);
+  });
+  $(".btn-category").click(function () {
+    var val = $(this).text();
+    $("#categories").tagsinput("add", val);
+  });
 });
